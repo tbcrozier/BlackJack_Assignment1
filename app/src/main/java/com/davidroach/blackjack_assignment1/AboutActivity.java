9bc6320e57141d640df6801c6cce1ffbbf06e5ad
droach-dev <mrroachhimself@gmail.com>
2017-01-26 19:18:40 -0600

    Renamed resource files and added two activities.

---
+++ app/src/main/java/com/davidroach/blackjack_assignment1/AboutActivity.java
@@ -0,0 +1,18 @@
+package com.davidroach.blackjack_assignment1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ * Created by droach-dev on 1/26/17.
+ */
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+
+
+}
---
+++ app/src/main/java/com/davidroach/blackjack_assignment1/GameActivity.java
@@ -0,0 +1,17 @@
+package com.davidroach.blackjack_assignment1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Created by droach-dev on 1/26/17.
+ */
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
