810a7ecbc160ec5e501ff53a6b5cbaeb969fd8bc
droach-dev <mrroachhimself@gmail.com>
2017-01-27 15:49:22 -0600

    Removed Title Bar and started activity skeletons.

--- app/src/main/java/com/davidroach/blackjack_assignment1/StartActivity.java
+++ app/src/main/java/com/davidroach/blackjack_assignment1/StartActivity.java
@@ -2,7 +2,9 @@ package com.davidroach.blackjack_assignment1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -14,5 +16,43 @@ public class StartActivity extends AppCompatActivity {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
         setContentView(R.layout.activity_start);
+
+        /* Buttons and their listensers */
+
+        final Button playButton = (Button) findViewById(R.id.play_button);
+
+        playButton.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v){
+
+            }
+
+        });
+
+
+        final Button aboutButton = (Button) findViewById(R.id.about_button);
+
+        aboutButton.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v){
+
+            }
+
+        });
+
+
+
+        final Button exitButton = (Button) findViewById(R.id.exit_button);
+
+        exitButton.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v){
+
+            }
+
+        });
+
+
+
     }
 }
