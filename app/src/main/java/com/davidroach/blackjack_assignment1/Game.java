1672d3ad9354e8cc38055bd77d3c744e58b21a14
droach-dev <mrroachhimself@gmail.com>
2017-01-28 14:24:20 -0600

    Added Player and Game class boiler code.
    Currently working on  activty_game.xml.

---
+++ app/src/main/java/com/davidroach/blackjack_assignment1/Game.java
@@ -0,0 +1,62 @@
+package com.davidroach.blackjack_assignment1;
+
+/**
+ * Created by droach-dev on 1/28/17.
+ */
+
+public class Game {
+
+    public int betSize;
+
+    /* need to figure out how the deck is going to work.
+       Will put variable here.
+     */
+
+
+    /* Constructor */
+    public Game(){
+
+    }
+
+    public void shuffleDeck(){
+
+    }
+
+    public void dealCard(){
+
+    }
+
+    public void takeHit(){
+
+    }
+
+    public void takeStand(){
+
+    }
+
+
+    //Pass hand variables to these functions.
+    //once you figure out its type
+    public void dealerCheckHand(){
+
+    }
+
+
+    public void placeBet(){
+
+    }
+
+    public void youWin(){
+
+    }
+
+    public void dealerWin(){
+
+    }
+
+
+
+
+
+
+}
