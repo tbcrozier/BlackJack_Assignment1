60c7e48a24658555497d4faa9344cadb7dee7aea
droach-dev <mrroachhimself@gmail.com>
2017-01-28 16:04:30 -0600

    Worked on activity_game.xml
    Currently working on  activty_game.xml.

--- app/src/main/java/com/davidroach/blackjack_assignment1/Game.java
+++ app/src/main/java/com/davidroach/blackjack_assignment1/Game.java
@@ -8,6 +8,63 @@ public class Game {
 
     public int betSize;
 
+    /* Will be used to show card in dynamic image view. */
+    public String[] cardFileNames = {
+            "ace_of_clubs.png",
+            "ace_of_diamonds.png",
+            "ace_of_hearts.png",
+            "ace_of_spades.png",
+            "eight_of_clubs.png",
+            "eight_of_diamonds.png",
+            "eight_of_hearts.png",
+            "eight_of_spades.png",
+            "five_of_clubs.png",
+            "five_of_diamonds.png",
+            "five_of_hearts.png",
+            "five_of_spades.png",
+            "four_of_clubs.png",
+            "four_of_diamonds.png",
+            "four_of_hearts.png",
+            "four_of_spades.png",
+            "jack_of_clubs.png",
+            "jack_of_diamonds.png",
+            "jack_of_hearts.png",
+            "jack_of_spades.png",
+            "king_of_clubs.png",
+            "king_of_diamonds.png",
+            "king_of_hearts.png",
+            "king_of_spades.png",
+            "nine_of_clubs.png",
+            "nine_of_diamonds.png",
+            "nine_of_hearts.png",
+            "nine_of_spades.png",
+            "queen_of_clubs.png",
+            "queen_of_diamonds.png",
+            "queen_of_hearts.png",
+            "queen_of_spades.png",
+            "seven_of_clubs.png",
+            "seven_of_diamonds.png",
+            "seven_of_hearts.png",
+            "seven_of_spades.png",
+            "six_of_clubs.png",
+            "six_of_diamonds.png",
+            "six_of_hearts.png",
+            "six_of_spades.png",
+            "ten_of_clubs.png",
+            "ten_of_diamonds.png",
+            "ten_of_hearts.png",
+            "ten_of_spades.png",
+            "three_of_clubs.png",
+            "three_of_diamonds.png",
+            "three_of_hearts.png",
+            "three_of_spades.png",
+            "two_of_clubs.png",
+            "two_of_diamonds.png",
+            "two_of_hearts.png",
+            "two_of_spades.png",
+
+    };
+
     /* need to figure out how the deck is going to work.
        Will put variable here.
      */
