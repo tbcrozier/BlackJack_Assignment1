c0ae471ab68e946f490854786bb0448584fb1f08
droach-dev <mrroachhimself@gmail.com>
2017-02-07 15:12:57 -0600

    Fixed bug that allowed hands first two cards in hand to be shown before bet is made.

--- app/src/main/java/com/davidroach/blackjack_assignment1/Deck.java
+++ app/src/main/java/com/davidroach/blackjack_assignment1/Deck.java
@@ -75,7 +75,7 @@ public class Deck implements Serializable {
 
 
 
-    //when a random number between 1 and 52 is picked access this array with that number.
+
     //the zeros represent aces and need to be proccessed for value.
     public int[] cardVals = {0,0,0,0,8,8,8,8,5,5,5,5,4,4,4,4,10,10,10,10,10,10,10,10,9,9,9,9,10,10,10,10,7,7,7,7,6,6,6,6,10,10,10,10,3,3,3,3,2,2,2,2};
 
